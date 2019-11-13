bdecf70d8fe86b4ae8c594755e06fb5b1167cf0a
GenchoDraganov <gencho.draganov.uni@gmail.com>
2019-11-13 23:38:39 +0200

    Updated the domain model again

--- Booking.Domain/ResortType.cs
+++ Booking.Domain/ResortType.cs
@@ -4,10 +4,10 @@ using System.Text;
 
 namespace Booking.Domain
 {
-    class ClientType
+    class ResortType
     {
         public byte Id { get; set; }
         public string Name { get; set; }
-        public byte DiscountRate { get; set; }
+        public byte Type { get; set; }
     }
 }
