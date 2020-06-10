649a4135ad17c50019ad0d333064bef22eece278
GenchoDraganov <gencho.draganov.uni@gmail.com>
2020-06-11 02:37:48 +0300

    Edited startup script

--- start_project.bat
+++ start_project.bat
@@ -2,4 +2,4 @@
 start cmd.exe /k "cd Booking.WebApi & echo Starting up the Back-End & dotnet run"
 
 ::Start Front-End
-start cmd.exe /k "cd ../Booking-FE & echo Starting up the Front-End & npm start & echo Front-End started successfully"
+start cmd.exe /k "cd ../Booking-FE & echo Starting up the Front-End & npm start"
